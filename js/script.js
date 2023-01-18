e83d0b9a64425706717a4a16754f2b5abe5ba9ec
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-01-18 13:38:53 +0600

    added code for mobile navigation

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
+// Mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
